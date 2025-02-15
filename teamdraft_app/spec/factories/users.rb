b7488988b2a8c27c23266375a3d3c0e2139bd367
Andrei Balbo <andreibalbo@gmail.com>
2025-02-15 13:27:52 -0300

    feat: Adjust user tests

--- teamdraft_app/spec/factories/users.rb
+++ teamdraft_app/spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "user#{n}@example.com" }
-    password { 'password123' }
-    # Add other required attributes here
+    password { "password123" }
+    password_confirmation { "password123" }
   end
 end
--- teamdraft_app/spec/models/user_spec.rb
+++ teamdraft_app/spec/models/user_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
+    subject { build(:user) }
+
     it { should validate_presence_of(:email) }
-    it { should validate_uniqueness_of(:email).case_insensitive }
     it { should validate_presence_of(:password) }
     it { should validate_confirmation_of(:password) }
+    it { should validate_length_of(:password).is_at_least(6) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
   end
 
   describe 'authentication' do
