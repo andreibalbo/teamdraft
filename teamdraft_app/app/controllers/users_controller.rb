51932a07d04b06d76155d11b69c736e9d626ad1f
Andrei Balbo <andreibalbo@gmail.com>
2025-02-15 11:54:10 -0300

    feat: Adjust db config and user regsiter

--- teamdraft_app/app/controllers/users_controller.rb
+++ teamdraft_app/app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    puts "User params: #{@user.inspect}"
 
     if @user.save
       session[:user_id] = @user.id
