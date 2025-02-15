81f87871ca6acd02845d58f3802a3108900a6659
Andrei Balbo <andreibalbo@gmail.com>
2025-02-15 12:10:31 -0300

    config: redirect user to login page if not logged in

--- teamdraft_app/app/controllers/application_controller.rb
+++ teamdraft_app/app/controllers/application_controller.rb
@@ -8,4 +8,10 @@ private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def authenticate_user!
+    unless current_user
+      redirect_to log_in_path, alert: "Please log in to access this page"
+    end
+  end
 end
