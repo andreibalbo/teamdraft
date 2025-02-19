034c0d13fa18676a4d9cd2d124ee5ded7a5b7bdf
Andrei Balbo <andreibalbo@gmail.com>
2025-02-19 15:56:11 -0300

    feat: Add basic tests

--- teamdraft_app/app/controllers/groups_controller.rb
+++ teamdraft_app/app/controllers/groups_controller.rb
@@ -3,51 +3,9 @@ class GroupsController < ApplicationController
 
   def index
     @groups = Group.all
-    respond_to do |format|
-      format.html
-      format.json { render json: @groups }
-    end
   end
 
   def show
-    respond_to do |format|
-      format.html
-      format.json { render json: @group }
-    end
-  end
-
-  def create
-    @group = Group.new(group_params)
-
-    respond_to do |format|
-      if @group.save
-        format.html { redirect_to @group, notice: "Group was successfully created." }
-        format.json { render json: @group, status: :created }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @group.update(group_params)
-        format.html { redirect_to @group, notice: "Group was successfully updated." }
-        format.json { render json: @group }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @group.destroy
-    respond_to do |format|
-      format.html { redirect_to groups_url, notice: "Group was successfully deleted." }
-      format.json { head :no_content }
-    end
   end
 
   def new
@@ -55,6 +13,29 @@ class GroupsController < ApplicationController
   end
 
   def edit
+  end
+
+  def create
+    @group = Group.new(group_params)
+
+    if @group.save
+      redirect_to @group, notice: "Group was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to @group, notice: "Group was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @group.destroy
+    redirect_to groups_url, notice: "Group was successfully deleted."
   end
 
 private
