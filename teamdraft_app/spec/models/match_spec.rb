ec1494038a9d1bc7149777186a0c2f91e3d2e552
Andrei Balbo <andreibalbo@gmail.com>
2025-02-22 16:46:46 -0300

    feat: add unit tests

--- teamdraft_app/spec/models/match_spec.rb
+++ teamdraft_app/spec/models/match_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Match, type: :model do
   describe 'associations' do
     it { should belong_to(:group) }
+    it { should have_many(:participations).dependent(:destroy) }
+    it { should have_many(:players).through(:participations) }
   end
 
   describe 'validations' do
--- teamdraft_app/spec/models/player_spec.rb
+++ teamdraft_app/spec/models/player_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Player, type: :model do
   describe 'associations' do
     it { should belong_to(:group) }
+    it { should have_many(:participations).dependent(:destroy) }
+    it { should have_many(:matches).through(:participations) }
   end
 
   describe 'validations' do
