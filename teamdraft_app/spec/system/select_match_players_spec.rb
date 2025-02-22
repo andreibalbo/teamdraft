ff5375f6f251882c0e6478f6cf241fbeb94457d4
Andrei Balbo <andreibalbo@gmail.com>
2025-02-22 17:10:36 -0300

    feat: Add a new test unchecking players

--- teamdraft_app/spec/system/select_match_players_spec.rb
+++ teamdraft_app/spec/system/select_match_players_spec.rb
@@ -31,4 +31,29 @@ RSpec.describe "Select Match Players", type: :system do
     expect(page).to have_content("Player 2")
     expect(page).to have_content("Participating Players (2)")
   end
+
+  it "allows admin to remove players from match" do
+    match.players << [ player1, player2 ]
+
+    visit match_path(match)
+    expect(page).to have_content("Participating Players (2)")
+
+    click_link "Select Players"
+
+    uncheck "player_#{player2.id}"
+    click_button "Update Players"
+
+    expect(page).to have_current_path(match_path(match))
+    expect(page).to have_content("Players updated successfully")
+    expect(page).to have_content("Player 1")
+    expect(page).not_to have_content("Player 2")
+    expect(page).to have_content("Participating Players (1)")
+
+    click_link "Select Players"
+    uncheck "player_#{player1.id}"
+    click_button "Update Players"
+
+    expect(page).to have_content("No players added to this match yet")
+    expect(page).to have_content("Participating Players (0)")
+  end
 end
