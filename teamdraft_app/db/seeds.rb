78559866b652873111ec7e1a19997173277997b8
Andrei Balbo <andreibalbo@gmail.com>
2025-02-23 00:13:18 -0300

    chore: Add better seeds

--- teamdraft_app/db/seeds.rb
+++ teamdraft_app/db/seeds.rb
@@ -23,3 +23,22 @@ Membership.create!(
   group: group,
   role: :admin
 )
+
+Player.create!(name: "And", group: group, positioning: 50, attack: 50, defense: 50, stamina: 50)
+Player.create!(name: "Iur", group: group, positioning: 60, attack: 40, defense: 60, stamina: 60)
+Player.create!(name: "Yag", group: group, positioning: 30, attack: 20, defense: 60, stamina: 40)
+Player.create!(name: "Bil", group: group, positioning: 100, attack: 70, defense: 10, stamina: 30)
+Player.create!(name: "Dem", group: group, positioning: 50, attack: 70, defense: 30, stamina: 30)
+Player.create!(name: "Fagn", group: group, positioning: 0, attack: 30, defense: 90, stamina: 50)
+Player.create!(name: "Franc", group: group, positioning: 0, attack: 20, defense: 90, stamina: 60)
+Player.create!(name: "Danil", group: group, positioning: 20, attack: 30, defense: 50, stamina: 50)
+Player.create!(name: "Facun", group: group, positioning: 100, attack: 50, defense: 0, stamina: 70)
+Player.create!(name: "Mati", group: group, positioning: 50, attack: 70, defense: 60, stamina: 70)
+Player.create!(name: "Raph", group: group, positioning: 30, attack: 40, defense: 40, stamina: 30)
+Player.create!(name: "LucP", group: group, positioning: 90, attack: 80, defense: 20, stamina: 40)
+Player.create!(name: "Dal", group: group, positioning: 40, attack: 50, defense: 40, stamina: 40)
+Player.create!(name: "Ramo", group: group, positioning: 20, attack: 30, defense: 70, stamina: 50)
+Player.create!(name: "JonathM", group: group, positioning: 20, attack: 30, defense: 60, stamina: 40)
+Player.create!(name: "Kelv", group: group, positioning: 70, attack: 40, defense: 30, stamina: 30)
+Player.create!(name: "Guil", group: group, positioning: 90, attack: 80, defense: 10, stamina: 50)
+Player.create!(name: "Vanil", group: group, positioning: 60, attack: 60, defense: 60, stamina: 60)
