f147a7434eaa3ee28a08d7033c3c7392f74845ba
Andrei Balbo <31902117+andreibalbo@users.noreply.github.com>
2025-03-14 10:45:27 -0300

    Config/ngrok host (#20)
    * feat: Improve authentication rules
    
    * feat: style group pages
    
    * feat: Add desktop version of group show
    
    * feat: add responsiveness to group show
    
    * feat: Make groups show responsive
    
    * feat: add basic styling to players and matches forms
    
    * feat:  Improve tables behavior
    
    * feat: add styling to match page
    
    * feat: Style signup page
    
    * feat: Style players selection screen
    
    * feat: Add a little bit of styling to draft show page
    
    * feat: Improving code separation on draft show
    
    * feat: add soccer court and divide sectors method
    
    * refactor: change lineup logic into a service
    
    * feat: Improve court and add gk
    
    * feat: Adjust court size and stretching
    
    * fix: Adjust player render and css
    
    * config: Accept ngrok config
    
    * chore: Improve seeds and layout

--- teamdraft_app/db/seeds.rb
+++ teamdraft_app/db/seeds.rb
@@ -15,6 +15,7 @@ admin = User.create!(
 
 group = Group.create!(
   name: "Fut Society segunda 18h30",
+  description: "Skinabol",
   category: :society
 )
 
@@ -24,21 +25,21 @@ Membership.create!(
   role: :admin
 )
 
-Player.create!(name: "And", group: group, positioning: 50, attack: 50, defense: 50, stamina: 50)
-Player.create!(name: "Iur", group: group, positioning: 60, attack: 40, defense: 60, stamina: 60)
-Player.create!(name: "Yag", group: group, positioning: 30, attack: 20, defense: 60, stamina: 40)
-Player.create!(name: "Bil", group: group, positioning: 100, attack: 70, defense: 10, stamina: 30)
-Player.create!(name: "Dem", group: group, positioning: 50, attack: 70, defense: 30, stamina: 30)
-Player.create!(name: "Fagn", group: group, positioning: 0, attack: 30, defense: 90, stamina: 50)
-Player.create!(name: "Franc", group: group, positioning: 0, attack: 20, defense: 90, stamina: 60)
-Player.create!(name: "Danil", group: group, positioning: 20, attack: 30, defense: 50, stamina: 50)
-Player.create!(name: "Facun", group: group, positioning: 100, attack: 50, defense: 0, stamina: 70)
-Player.create!(name: "Mati", group: group, positioning: 50, attack: 70, defense: 60, stamina: 70)
-Player.create!(name: "Raph", group: group, positioning: 30, attack: 40, defense: 40, stamina: 30)
-Player.create!(name: "LucP", group: group, positioning: 90, attack: 80, defense: 20, stamina: 40)
-Player.create!(name: "Dal", group: group, positioning: 40, attack: 50, defense: 40, stamina: 40)
-Player.create!(name: "Ramo", group: group, positioning: 20, attack: 30, defense: 70, stamina: 50)
-Player.create!(name: "JonathM", group: group, positioning: 20, attack: 30, defense: 60, stamina: 40)
-Player.create!(name: "Kelv", group: group, positioning: 70, attack: 40, defense: 30, stamina: 30)
-Player.create!(name: "Guil", group: group, positioning: 90, attack: 80, defense: 10, stamina: 50)
-Player.create!(name: "Vanil", group: group, positioning: 60, attack: 60, defense: 60, stamina: 60)
+Player.create!(name: "Andrei", group: group, positioning: 50, attack: 50, defense: 50, stamina: 50)
+Player.create!(name: "Iuri", group: group, positioning: 60, attack: 40, defense: 60, stamina: 60)
+Player.create!(name: "Yago", group: group, positioning: 30, attack: 20, defense: 60, stamina: 40)
+Player.create!(name: "Bill", group: group, positioning: 100, attack: 70, defense: 10, stamina: 30)
+Player.create!(name: "Deme", group: group, positioning: 50, attack: 70, defense: 30, stamina: 30)
+Player.create!(name: "Fagner", group: group, positioning: 0, attack: 30, defense: 90, stamina: 50)
+Player.create!(name: "Francys", group: group, positioning: 0, attack: 20, defense: 90, stamina: 60)
+Player.create!(name: "Danilo", group: group, positioning: 20, attack: 30, defense: 50, stamina: 50)
+Player.create!(name: "Facundo", group: group, positioning: 100, attack: 50, defense: 0, stamina: 70)
+Player.create!(name: "Matias", group: group, positioning: 50, attack: 70, defense: 60, stamina: 70)
+Player.create!(name: "Raphael", group: group, positioning: 30, attack: 40, defense: 40, stamina: 30)
+Player.create!(name: "Lucas P", group: group, positioning: 90, attack: 80, defense: 20, stamina: 40)
+Player.create!(name: "Dalle", group: group, positioning: 40, attack: 50, defense: 40, stamina: 40)
+Player.create!(name: "Ramon", group: group, positioning: 20, attack: 30, defense: 70, stamina: 50)
+Player.create!(name: "Jonathan", group: group, positioning: 20, attack: 30, defense: 60, stamina: 40)
+Player.create!(name: "Kelvin", group: group, positioning: 70, attack: 40, defense: 30, stamina: 30)
+Player.create!(name: "Guilherme", group: group, positioning: 90, attack: 80, defense: 10, stamina: 50)
+Player.create!(name: "Vanildo", group: group, positioning: 60, attack: 60, defense: 60, stamina: 60)
